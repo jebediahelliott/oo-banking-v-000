c5584c0fa79e601334e091e584a5a4c80b49c487
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-04 14:27:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.sender.valid? != true
+    if self.sender.valid? 
       "Transaction rejected. Please check your account balance."
       self.status = "rejected"
     elsif self.valid?
